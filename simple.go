27d1751954f3015cc193794a4577447219675e65
Beyang Liu <beyang.liu@gmail.com>
2016-03-16 19:30:14 -0700

    add docs

--- simple.go
+++ simple.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+// main runs the program.
 func main() {
 	myfunc()
 	fmt.Println("Hello world")
 }
 
+// myfunc is a function that does nothing.
 func myfunc() {
 
 }
